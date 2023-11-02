76df6a9629c371923bc12c393707e52a50e8fda7
Praveen Paliwal <praveenpaliwal273@gmail.com>
2023-11-02 22:17:03 +0000

    identifying unique labels and tags

---
+++ data_prep2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import csv
+
+df = pd.read_csv('Stackoverflow_Question_Classification_Challenge/full_data.csv',sep=';')
+# print(df.describe())
+labels_list= df['label'].unique()
+# print(df['label'].count())
+# print(df['label'].value_counts())
+print('unique_labels:',labels_list)
+# print(df['tags'].count())
+# print(df['tags'].value_counts())
+tags_list= df['tags'].unique()
+print('unique_tags:',tags_list)
+
+# print()
+
+# tags_list = df['tags'].unique()
+# print('unique_tags:',tags_list)
+
+
+
+
