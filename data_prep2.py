b99e93460819dd1590e15d4c3c0a72639fe7a761
Praveen Paliwal <praveenpaliwal273@gmail.com>
2023-11-28 22:52:28 +0000

    Replacing column "tags" by length of number of tags

--- data_prep2.py
+++ data_prep2.py
@@ -37,11 +37,16 @@ else:
 print(question_for_label)
 print(question_for_label.count())
 
-''''Determining length of question'''
+'''Determining length of question'''
 y = lambda x: len(x.split(' '))
 new_df = df
 length = new_df['title'].apply(y)
 new_df.insert(column='title_length', value=length, loc=1)
 print(new_df.head())
 
-
+'''Replacing column "tags" by length of number of tags'''
+print(df.head())
+tag_length = new_df['tags'].apply(y)
+new_df.insert(column='len_tags', value=tag_length, loc=2)
+tag_df = new_df.drop('tags',axis = 1)
+print(tag_df.head())
